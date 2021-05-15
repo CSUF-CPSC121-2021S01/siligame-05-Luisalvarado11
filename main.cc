73c4bd60e840024926de7f1fd14586059a8b5175
Luisalvarado11 <70175055+Luisalvarado11@users.noreply.github.com>
2021-05-14 20:51:42 -0700

    Update main.cc

---
+++ main.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "cpputils/graphics/image.h"
+#include "game.h"
+#include "game_element.h"
+
+int main() {
+  Game game_;
+  game_.Init();
+  game_.Start();
+
+  return 0;
+}
