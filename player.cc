3abe3ce62535028922911bb8fa3fc4d30c84f984
Luisalvarado11 <70175055+Luisalvarado11@users.noreply.github.com>
2021-05-14 20:50:35 -0700

    Update player.cc

---
+++ player.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "cpputils/graphics/image.h"
+#include "opponent.h"
+#include "player.h"
+
+//@breif: Function that draws the Image, based on its width(50) and height(50);
+//@param: Takes an Referance Image
+//@return: void
+void Player::Draw(graphics::Image &playerimage) {
+  graphics::Image Hero;
+  Hero.Load("player.bmp");
+  for (int i = 0; i < GetWidth(); i++) {
+    for (int j = 0; j < GetHeight(); j++) {
+      playerimage.SetColor(GetX() + i, GetY() + j, Hero.GetColor(i, j));
+    }
+  }
+}
+
+void Player::Move(const graphics::Image &playerimage) {}
+
+//@breif: Function that draws the Image, based on its width(50) and height(50);
+//@param: Takes an Referance Image
+//@return: void
+void PlayerProjectile::Draw(graphics::Image &p_projectileimage) {
+  graphics::Image Hero_projectile;
+  Hero_projectile.Load("p_projectile.bmp");
+  for (int i = 0; i < GetWidth(); i++) {
+    for (int j = 0; j < GetHeight(); j++) {
+      p_projectileimage.SetColor(GetX() + i, GetY() + j,
+                                 Hero_projectile.GetColor(i, j));
+    }
+  }
+}
+
+void PlayerProjectile::Move(const graphics::Image &p_projectileimage) {
+  SetX(GetX() - 3);
+  SetY(GetY() - 3);
+
+  if (IsOutOfBounds(p_projectileimage)) {
+    SetIsActive(false);
+  } else {
+    SetIsActive(true);
+  }
+}
